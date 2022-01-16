132bac730285a72b581ada887a4ab310fb0f6c99
sharonytlau <sharonytlau@gmail.com>
2022-01-16 23:25:32 +0800

    Add UI

--- script.js
+++ script.js
@@ -9,22 +9,14 @@ function computerPlay(){
 
 // single round of Rock Paper Scissors, return 1 if player wins, return 0 if tie, else return -1
 function singleRound(playerSelection, computerSelection){
-  console.log(`Your selection: ${playerSelection}. Computer Selection: ${computerSelection}.`)
 
   if (playerSelection === computerSelection){
-    console.log("Tie!");
     return 0
   }
 
   for (let p = 0; p < selectionPairs.length; p++){
     if (playerSelection === selectionPairs[p][0]){
-      if (computerSelection === selectionPairs[p][1]){
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        return 1;
-      } else {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-        return -1;
-      }
+      return (computerSelection === selectionPairs[p][1]) ? 1 : -1; 
     }
   }
 
@@ -34,6 +26,8 @@ function singleRound(playerSelection, computerSelection){
 function checkInput(inputString){
   return choices.includes(inputString.toLowerCase()) ? true : false
 }
+
+/*
 
 // game 5 round game that keeps score and reports a winner or loser at the end
 function game(){
@@ -72,4 +66,121 @@ function game(){
 
 }
 
-game();
+*/
+
+// game();
+
+// new
+
+const selectionBtns = document.querySelectorAll(".btn-selection");
+const body = document.querySelector("body");
+
+const playerAvatar = document.querySelector("#avatar");
+const playerScoreDiv = document.querySelector("#score-player");
+const computerScoreDiv = document.querySelector("#score-computer");
+
+const computerSelectionDiv = document.querySelector("#computer-selection")
+
+const resultDiv = document.querySelector("#game-result"); 
+
+function getSelection(e){
+  playerSelection = e.target.id;
+  return playerSelection
+}
+let playerScore = 0;
+let computerScore = 0;
+
+function updateComputerDiv(computerSelection){
+  switch (computerSelection){
+    case "rock":
+      computerSelectionDiv.textContent = "✊";
+      break;
+    case "paper":
+      computerSelectionDiv.textContent = "🖐";
+      break;
+    case "scissors":
+      computerSelectionDiv.textContent = "✌";
+      break;
+  }
+}
+
+function game(e){
+
+  
+  computerSelectionDiv.style["background-color"] = "var(--color-primary)";
+
+  let playerSelection = getSelection(e);
+  let computerSelection = computerPlay();
+
+  setTimeout(() => { 
+    computerSelectionDiv.style["background-color"] = "var(--color-primary-light)";
+  }, 100);
+
+  setTimeout(() => { 
+    computerSelectionDiv.style["border"] = "5px solid var(--color-dark-gray)";
+  }, 100);
+
+
+  setTimeout(updateComputerDiv(computerSelection), 200);
+
+  computerSelectionDiv.style["border"] = "5px solid var(--color-primary)";
+  
+
+  // computerSelectionDiv.style["background-color"] = "var(--color-primary-light)";
+
+
+
+
+  roundResult = singleRound(playerSelection, computerSelection);
+  if (roundResult > 0){
+    playerAvatar.textContent = "😎";
+    playerScore += roundResult;
+  } else if (roundResult === 0){
+    playerAvatar.textContent = "🧐"; 
+  } else {
+    playerAvatar.textContent = "🤔";
+    computerScore += -roundResult;
+  }
+  
+  playerScoreDiv.textContent = playerScore;
+  computerScoreDiv.textContent = computerScore;
+
+  if (playerScore === 5 || computerScore === 5) {
+
+    if (playerScore > computerScore){
+      playerAvatar.textContent = '😊';
+      resultDiv.textContent = "You win!";
+    } else {
+      playerAvatar.textContent = '🥲';
+      resultDiv.textContent = "Computer wins!";
+    } 
+
+    selectionBtns.forEach(item => item.removeEventListener('click', game));
+  }
+
+}
+
+startNewGame();
+
+function startNewGame(){
+
+  playerScoreDiv.textContent = 0;
+  computerScoreDiv.textContent = 0;
+
+  playerScore = 0;
+  computerScore = 0;
+
+  resultDiv.textContent = '';
+  playerAvatar.textContent = '😀';
+      
+  selectionBtns.forEach(item => item.addEventListener('click', game)); 
+
+  computerSelectionDiv.style["background-color"] = "var(--color-light-gray)";
+  computerSelectionDiv.textContent = '';
+}
+
+const restartBtn = document.querySelector("#restart-btn");
+restartBtn.addEventListener('click', startNewGame);
+
+
+// console.log(singleRound());
